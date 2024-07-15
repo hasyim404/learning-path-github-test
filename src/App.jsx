8dd1e66af2abfc4890fed72cad955a6b7ec48fb8
Muhammad Ismail <72483245+hasyim404@users.noreply.github.com>
2024-07-15 12:01:58 +0000

    change props

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Ismail",
+  title: "Junior Web Developer",
+  email: "muhaisim7@gmail.com",
+  gitHub: "hasyim404",
+  instagram: "muhh.ismail__",
+  linkedIn: "mu-ismail",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
